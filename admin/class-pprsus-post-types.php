8e1bd2153c898f0e2c3e72e8384e85caf0c3c80c
childressdev <jcampbell@childressagency.com>
2019-06-07 16:30:29 -0400

    addedsecurity cpt

--- admin/class-pprsus-post-types.php
+++ admin/class-pprsus-post-types.php
@@ -68,7 +68,38 @@ if(!class_exists('PPRSUS_Post_Types')){
           'author'
         )
       );
-      register_post_type('medical_history', $medical_history_args);      
+      register_post_type('medical_history', $medical_history_args); 
+      
+      $security_labels = array(
+        'name' => esc_html_x('Security Profiles', 'post type general name' , 'pprsus'),
+        'singular_name' => esc_html_x('Security Profile', 'post type singular name', 'pprsus'),
+        'menu_name' => esc_html_x('Security Profiles', 'post type menu name', 'pprsus'),
+        'add_new_item' => esc_html__('Add New Security Profile', 'pprsus'),
+        'search_items' => esc_html__('Search Security Profiles', 'pprsus'),
+        'edit_item' => esc_html__('Edit Security Profile', 'pprsus'),
+        'view_item' => esc_html__('View Security Profile', 'pprsus'),
+        'all_items' => esc_html__('All Security Profiles', 'pprsus'),
+        'new_item' => esc_html__('New Security Profile', 'pprsus'),
+        'not_found' => esc_html__('No security Profiles Found', 'pprsus')
+      );
+
+      $security_args = array(
+        'labels' => $security_labels,
+        'capability_type' => 'post',
+        'public' => true,
+        'menu_position' => 7,
+        'menu_icon' => 'dashicons-shield-alt',
+        'query_var' => 'security',
+        'has_archive' => false,
+        'show_in_rest' => true,
+        'supports' => array(
+          'title',
+          'custom-fields',
+          'revisions',
+          'author'
+        );
+        register_post_type('security', $security_args);
+      );
     }//end create_post_types()
 
   }
