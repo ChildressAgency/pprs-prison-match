39a551d83871158cd6efa3e736f550e3c29e9c63
childressdev <jcampbell@childressagency.com>
2019-07-12 10:44:06 -0400

    added table

--- templates/loop/matched-prisons.php
+++ templates/loop/matched-prisons.php
@@ -5,9 +5,25 @@
 if(!defined('ABSPATH')){ exit; }
 ?>
 
-<?php foreach($prison_list as $prison): ?>
-<div class="prison">
-  <h3><?php echo esc_html(get_the_title($prison->ID)) . ' id=' . $prison->ID; ?></h3>
-  
+<div class="dashboard">
+  <div class="table-responsive">
+    <table id="prison-list" class="table table-striped">
+      <thead>
+        <tr>
+          <th><?php echo esc_html__('Prison Name', 'pprsus'); ?></th>
+          <th><?php echo esc_html__('Distance', 'pprsus'); ?></th>
+          <th><?php echo esc_html__('More Information', 'pprsus'); ?></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach($prison_list as $prison): ?>
+          <tr>
+            <td><?php echo esc_html(get_the_title($prison->ID)); ?></td>
+            <td><?php echo round($prison->distance) . ' miles'; ?></td>
+            <td><a href="<?php echo esc_url(get_permalink($prison->ID)); ?>">View More</a></td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  </div>
 </div>
-<?php endforeach;
